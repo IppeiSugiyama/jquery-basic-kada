f4b7736212023df3f22d2e6ece35b3d00c53896e
IppeiSugiyama <kani.ng.kane.ok@gmail.com>
2025-01-24 15:19:21 +0900

    課題004提出

---
+++ kadai_004/event.js
@@ -0,0 +1,21 @@
+// jQuery 「load」「scroll」のイベント処理を使用して以下の文字列をコンソールに表示してください。
+
+// HTMLドキュメントが読み込み完了したときに「loadイベントが発生しました」とコンソールに出力する
+// 画面をスクロールしたときに「scrollイベントが発生しました」とコンソールに出力する
+// 作業手順は後述の各Stepを参考にしてください。
+
+// ※参考コード
+
+// $(window).on(イベント, () => {});
+
+$(function() {
+
+  // HTMLが読み込まれたとき
+  $(document).ready(function(){
+    console.log('loadイベントが発生しました');
+   });
+   
+   $(window).on('scroll',function(){
+    console.log('scrollイベントが発生しました');
+   });
+});
